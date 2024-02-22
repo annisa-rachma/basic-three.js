f8bfb873cf23b085ce7378da875415ac3c79b85e
annisarachma <saa.annisa15@gmail.com>
2024-02-22 23:49:24 +0700

    scale

--- src/script.js
+++ src/script.js
@@ -10,7 +10,9 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-/**
+scene.add(mesh)
+
+/**POSITIONING
  * u can put this position anywhere, ngga tergantung sama peletakan selama masih di atas renderer.render
  */
 // mesh.position.x = 0.7
@@ -20,16 +22,20 @@ const mesh = new THREE.Mesh(geometry, material)
 //set method -> to pass each property x,y,z 
 mesh.position.set(0.7,-0.6,1)
 
-scene.add(mesh)
+/**SCALE */
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2,0.5,0.5)
 
+/**NORMALIZE */
 // mesh.position.normalize()
 // normalize it value -> ngejadiin mesh jadi 1 
 
+/**AXES HELPER */
 //Axes Helper -> garis axis pembantu
-/**
 const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
-*/
 
 
 //sizes
@@ -44,7 +50,6 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 //add camera to scene is optional, but if you dont, it might result in a bug in some situation, better add
-
 
 
 //renderer
