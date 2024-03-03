23ad6621b0428d1bca3da81a07b7e5324f118630
annisarachma <saa.annisa15@gmail.com>
2024-03-03 14:14:18 +0700

    transform using group

--- src/script.js
+++ src/script.js
@@ -60,11 +60,35 @@ const scene = new THREE.Scene()
 
 /************************************* */
 
-
+//create group
 const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+//add group to the scene
 scene.add(group)
 
+//create object
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+//add object to the group
+group.add(cube1)
 
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 /**NORMALIZE */
 // mesh.position.normalize()
