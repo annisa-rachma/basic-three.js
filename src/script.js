bee1ee87e09fbbcabf9740d7b15054cf09190ba0
annisarachma <saa.annisa15@gmail.com>
2024-03-05 21:24:14 +0700

    add css styling

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import './style.css'
+// import './style.css'
 import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
@@ -124,8 +124,8 @@ scene.add(axesHelper)
 
 //sizes
 const sizes = {
-  width : 800,
-  height : 600
+  width : window.innerWidth,
+  height : window.innerHeight
 }
 
 //camera
